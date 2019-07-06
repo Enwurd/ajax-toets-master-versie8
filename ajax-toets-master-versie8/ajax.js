62b73ca43ff02664ea2dccc723b3abdf1ab93179
Enwurd <48202074+Enwurd@users.noreply.github.com>
2019-07-06 12:38:49 +0200

    Add files via upload

---
+++ ajax-toets-master-versie8/ajax.js
@@ -0,0 +1,7 @@
+/*
+  IK HEB DIT .JS BESTAND NIET GEBRUIKT, IK HEB EEN SCRIPT GERUND BINNEN INDEX.HTML, DIT WAS DE ENIGE MANIER WAAROP IK DE AJAX KON LATEN WERKEN.
+  naam en klas staan in index.HTML
+  verder kreeg ik het niet voor elkaar om het opgehaalde bestand in een ander format dan tekst te krijgen, alleen ik ben hier al zoveel tijd aan kwijt dat ik het opgegeven heb. de ajaxrequest werkt.
+ */
+
+let games_tabel = document.querySelector('#games-list');
